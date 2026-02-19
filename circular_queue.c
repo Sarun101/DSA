ca0b8e8334fd2d715c34147c46b65571ee04d9df
Sarun Balami <081csit38.sarun@scst.edu.np>
2026-02-19 16:39:30 +0545

    Update circular_queue.c

--- circular_queue.c
+++ circular_queue.c
@@ -1,82 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 #define MAX 5
-int front =-1, rear =-1;
+
+int front = -1, rear = -1;
 int queue[MAX];
 
 void Display(){
-    if (front == -1) {
-        printf("The queue is empty\n");
+    if(front == -1){
+        printf("Queue is empty\n");
         return;
     }
 
     int i = front;
-    while (1) {
-        printf("| %d |", queue[i]);
-        if (i == rear)
+    printf("Queue: ");
+
+    while(1){
+        printf("%d ", queue[i]);
+        if(i == rear)
             break;
         i = (i + 1) % MAX;
     }
+    printf("\n");
 }
 
-
 void Enqueue(){
-    if(front == (rear+1)%MAX){
-        printf("The Queue is FULL!\n");
+    if(front == (rear + 1) % MAX){
+        printf("Queue Overflow\n");
         return;
-        
     }
-    if(front==-1){
-        front=0;
-    }
-    rear = (rear+1)%MAX;
-    printf("Enter the data :");
-    scanf("%d",&queue[rear]);
+
+    if(front == -1)
+        front = 0;
+
+    rear = (rear + 1) % MAX;
+
+    printf("Enter value: ");
+    scanf("%d", &queue[rear]);
 
     Display();
 }
 
 void Dequeue(){
-    if(front==-1){
-        printf("The Queue is empty!\n");
+    if(front == -1){
+        printf("Queue Underflow\n");
         return;
     }
-    else if(front==rear){
-        printf("| %d | is deleted\n",queue[front]);
+
+    printf("Removed: %d\n", queue[front]);
+
+    if(front == rear){
         front = -1;
-        return;
+        rear = -1;
     }
-    printf("| %d | is deleted\n",queue[front]);
-    front = (front+1)%MAX;
+    else{
+        front = (front + 1) % MAX;
+    }
 
     Display();
-
 }
 
 int main(){
     int choice;
-    while(1){
 
-        printf("\n\n---------->MENU<----------\n\n");
-        printf("1.Enqueue\n");
+    while(1){
+        printf("\n1.Enqueue\n");
         printf("2.Dequeue\n");
         printf("3.Display\n");
         printf("4.Count\n");
         printf("5.Exit\n");
-        printf("\n\n---------->OPTS<----------\n\n");
-
-        printf("Enter the choice: ");
-        scanf("%d",&choice);
+        printf("Choice: ");
+        scanf("%d", &choice);
 
         switch(choice){
-
-            case 1: Enqueue();break;
-            case 2: Dequeue();break;
-            case 3: Display();break;
-            case 4: printf("Count");break;
-            case 5: printf("Exiting.....");exit(0);
-
-            default: printf("Invalid Choice!");
-        }
-    }
-}
+            case 1: Enqueue(); break;
+            case 2: Dequeue(); break;
+            case 3: Display(); break;
+            case 4:
+                if(front == -1)
+                    printf("Count: 0\n");
+                else if(rear >= front)
+                    printf("Count: %d\n", rear - front + 1);
+                else
+                    printf("Count: %d\n", MAX - front + rear + 1);
+                bre
